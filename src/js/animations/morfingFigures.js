4a43e3664dcbe6fc108c24e43577dbdca7845018
axe280 <axeworkmail@gmail.com>
2026-03-10 15:24:02 +0100

    improve figures animation

--- src/js/animations/morfingFigures.js
+++ src/js/animations/morfingFigures.js
@@ -1,6 +1,5 @@
 import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
-import { createSplitTextAnamition } from "./textEffects";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -19,14 +18,13 @@ export const initMorfingFigures = () => {
     ScrollTrigger.create({
       trigger: el,
       pin: figuresWrapper,
-      end: "bottom bottom",
+      end: `bottom bottom-=${sections[0].offsetHeight}`,
       // markers: true,
     });
 
     sections.forEach((section, index) => {
-      const textElems = section.querySelectorAll(
-        ".js-figure-effect-text > div",
-      );
+      const isLastSection = index === sections.length - 1;
+      const isFirstSection = index === 0;
 
       const figuresTl = gsap.timeline({
         scrollTrigger: {
@@ -35,21 +33,19 @@ export const initMorfingFigures = () => {
           end: "bottom top",
           // markers: true,
           scrub: true,
-          pin: index !== sections.length - 1,
+          pin: true,
           pinSpacing: false,
           onEnter: () => updateColors(section),
           onEnterBack: () => updateColors(section),
         },
       });
 
-      if (index === 0) {
+      if (isFirstSection) {
         figuresTl
           .to(fig1, { rotate: 45, xPercent: 100 }, 0)
           .to(fig3, { rotate: -45, xPercent: -100 }, 0)
           .to(fig2, { opacity: 0, duration: 0.1 }, "-=0.1")
           .set(fig3, { opacity: 0 });
-
-        gsap.set(textElems, { y: 0, opacity: 1 });
       } else if (index === 1) {
         figuresTl
           .to({}, { duration: 0.2 })
@@ -65,9 +61,19 @@ export const initMorfingFigures = () => {
           });
       }
 
-      ScrollTrigger.refresh();
-
+      // animate section inside blocks
+      const textElems = section.querySelectorAll(
+        ".js-figure-effect-text > div",
+      );
+      const showElems = () => {
+        gsap.set(textElems, { opacity: 1 });
+      };
       const textDuration = 0.5;
+
+      gsap.set(textElems, { opacity: 0 });
+      if (isFirstSection) {
+        showElems();
+      }
 
       ScrollTrigger.create({
         trigger: section,
@@ -75,33 +81,34 @@ export const initMorfingFigures = () => {
         end: "bottom top",
         // markers: true,
         onEnter: () => {
-          if (index === 0) return;
+          if (isFirstSection) return;
 
+          showElems();
           gsap.fromTo(
             textElems,
-            { y: 100, opacity: 0 },
-            { y: 0, opacity: 1, duration: textDuration },
+            { yPercent: 100 },
+            { yPercent: 0, duration: textDuration },
           );
         },
         onLeave: () => {
+          if (isLastSection) return;
           gsap.to(textElems, {
-            y: -100,
-            opacity: 0,
+            yPercent: -100,
             duration: textDuration,
           });
         },
         onEnterBack: () => {
+          if (isLastSection) return;
           gsap.fromTo(
             textElems,
-            { y: -100, opacity: 0 },
-            { y: 0, opacity: 1, duration: textDuration },
+            { yPercent: -100 },
+            { yPercent: 0, duration: textDuration },
           );
         },
         onLeaveBack: () => {
-          if (index === 0) return;
+          if (isFirstSection) return;
           gsap.to(textElems, {
-            y: 100,
-            opacity: 0,
+            yPercent: 100,
             duration: textDuration,
           });
         },
